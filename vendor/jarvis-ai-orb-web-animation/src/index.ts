bf5af4f9d904b64eeadfd59d0a83d47045618a6a
yashbisht99 <yash26684@gmail.com>
2026-07-12 17:12:15 +0530

    Add vendor/jarvis-ai-orb-web-animation/src/index.ts

---
+++ vendor/jarvis-ai-orb-web-animation/src/index.ts
@@ -0,0 +1,19 @@
+export { JarvisOrb } from "./JarvisOrb";
+export type { JarvisOrbProps, JarvisQuality } from "./JarvisOrb";
+export {
+  PALETTES,
+  STATE_TARGETS,
+  SIZE_PRESETS,
+  resolvePalette,
+  resolveStateTarget,
+} from "./states";
+export type {
+  JarvisState,
+  JarvisStateName,
+  JarvisStateTarget,
+  JarvisPalette,
+  JarvisPaletteName,
+  JarvisPaletteValues,
+  JarvisSize,
+  JarvisSizePreset,
+} from "./states";
